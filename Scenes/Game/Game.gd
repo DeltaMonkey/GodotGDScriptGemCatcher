08973af4214d35201f66b44b82d00e2d9e80d943
DeltaMonkey <engincanozkan@outlook.com>
2025-09-23 23:44:50 +0300

    Sound

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
-
+const EXPLODE = preload("res://Assets/explode.wav")
 const GEM = preload("res://Scenes/Gem/Gem.tscn")
 const MARGIN: float = 70.0
 
+
 @onready var spawn_timer: Timer = $SpawnTimer
 @onready var paddle: Area2D = $Paddle
+@onready var score_sound: AudioStreamPlayer2D = $ScoreSound
+@onready var sound: AudioStreamPlayer = $Sound
+
 
 func _ready() -> void:
 	spawn_gem()
@@ -22,6 +26,9 @@ func spawn_gem() -> void:
 
 
 func stop_all() -> void:
+	sound.stop()
+	sound.stream = EXPLODE
+	sound.play()
 	spawn_timer.stop()
 	paddle.set_process(false)
 	for child in get_children():
@@ -30,6 +37,9 @@ func stop_all() -> void:
 
 func _on_paddle_area_entered(area: Area2D) -> void:
 	print("Collision ", area)
+	if score_sound.playing == false:
+		score_sound.position = area.position
+		score_sound.play()
 
 
 func _on_gem_gem_off_screen() -> void:
